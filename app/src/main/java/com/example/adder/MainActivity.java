39f7b09a9a893abf8d69a910faeb162cf228daa7
Shreya <thisisshreya1998@gmail.com>
2024-04-19 17:09:36 +0530

    1-02

--- app/src/main/java/com/example/adder/MainActivity.java
+++ app/src/main/java/com/example/adder/MainActivity.java
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
@@ -147,7 +150,20 @@ public class MainActivity extends AppCompatActivity {
         //CODE TO ADD DATA END.....................................................
 
         //CODE TO REMOVE PARTICULAR DATA
-        /*noteRef.whereIn("DESIGNATION", Arrays.asList("Deputy General Manager","General Manager")).whereNotEqualTo("ADDRESS","NHIDCL HQrs (New Delhi)").delete()
+        /*noteRef.whereIn("DESIGNATION", Arrays.asList("Deputy General Manager","General Manager")).whereNotEqualTo("ADDRESS","NHIDCL HQrs (New Delhi)").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+                delete()
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
